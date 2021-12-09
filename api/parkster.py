80831bca9c4e749cdd6bd8b34391fb97510d3aa3
Martin Sjöborg <martin@sjoborg.org>
2021-12-09 10:54:31 +0100

    Parkster done!

--- api/parkster.py
+++ api/parkster.py
@@ -1,47 +1,62 @@
 import requests, base64, time
 
 def get_token(email, password):
+    '''
+    Returns the base64 token for the given `email` and `password`.
+    '''
     string = email + ":" + password
     token = base64.b64encode(string.encode('ascii')).decode()
     return token
 
 def buy(parkingzone_id,feezone_id,car_id,token,user_id):
-    url = "http://api.parkster.se/api/mobile/v2/parkings/short-term"
+    '''
+    Buys a ticket. 
+    Needs `parkingzone_id` from `search_by_query()` or `search_by_location()`,
+    `feezone_id` from `get_fee_id()`,
+    `user_id` and `car_id` from `get_user_id_and_cars()`,
+    and `token` from `get_token()`.
+    '''
 
-    querystring = {"parkingZoneId":parkingzone_id,"feeZoneId":feezone_id,"carId":car_id,"paymentAccountId":f"PRIVATE%3A{user_id}","timeout":"30"}
+    url = "https://api.parkster.se/api/mobile/v2/parkings/short-term"
 
-    payload = {'version':'321',
+    payload = f"parkingZoneId={parkingzone_id}&feeZoneId={feezone_id}&carId={car_id}&timeout=30&paymentAccountId=PRIVATE%3A{user_id}"
+    
+    querystring = {'version':'321',
     'platform':'android',
-    'platformVersion':'24',
-    'locale':'en_US',
-    'clientTime':int(time.time()),
-    'debugPhoneModel':'unknown_Android SDK built for x86_64_f62tlYkBQqOLKeWO55-vut'}
+    'platformVersion':'29',
+    'locale':'sv_SV',
+    'clientTime':int((time.time())*1e3),
+    'debugPhoneModel':'Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ'}
 
     headers = {
         "authorization": f"Basic {token}",
         "accept": "application/json",
         "host": "api.parkster.se",
         "connection": "Keep-Alive",
+        "content-type": "application/x-www-form-urlencoded",
         "accept-encoding": "gzip",
         "user-agent": "okhttp/3.14.9"
     }
 
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
     assert response.status_code == 200, print('Error in buy', response.status_code, response.text)
-    print(response.text)
     status = 'confirmed'
     return status
 
-def verify(token):
+def get_latest_ticket(token):
+    '''
+    Returns `(amount, currency)` of the latest ticket the user has bought.
+    '''
 
     url = "https://api.parkster.se/api/mobile/v2/latestparkings"
 
     querystring = {"version":"321",
                    "platform":"android",
-                   "platformVersion":"24",
-                   "locale":"en_US",
-                   "clientTime":int(time.time()),
-                   "debugPhoneModel":"unknown_Android SDK built for x86_64_f62tlYkBQqOLKeWO55-vut"}
+                   "platformVersion":"29",
+                   "locale":"sv_SV",
+                   "clientTime":int(time.time()*1e3),
+                   "debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
 
     headers = {
         "accept": "application/json",
@@ -53,15 +68,18 @@ def verify(token):
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
+    latest_ticket = response.json()['parkings'][0]
+    return latest_ticket['paymentAmount'] , latest_ticket['paymentCurrency']['symbol']
 
 
 def get_fee_id(parkingzone_id, token):
-    import requests
+    '''
+    Returns the fee zone id for the given `parkingzone_id`.
+    '''
 
     url = f"https://api.parkster.se/api/mobile/v2/parking-zones/{parkingzone_id}"
 
-    querystring = {"version":"321","platform":"android","platformVersion":"29","locale":"en_US","clientTime":int(time.time()),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
+    querystring = {"version":"321","platform":"android","platformVersion":"29","locale":"sv_SV","clientTime":int(time.time()*1e3),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
 
     headers = {
         "accept": "application/json",
@@ -78,6 +96,9 @@ def get_fee_id(parkingzone_id, token):
     return feezone_id
 
 def search_by_query(query, token, user_id):
+    '''
+    Returns a list of parking zones matching the `query`. Each element is a tuple with `(zone_name, zone_adress, zone_id)`
+    '''
     url = "https://api.parkster.se/api/mobile/v2/parking-zones/search"
 
     querystring = {"query":query,
@@ -85,10 +106,10 @@ def search_by_query(query, token, user_id):
                    "userId":user_id,
                    "version":"321",
                    "platform":"android",
-                   "platformVersion":"24",
-                   "locale":"en_US",
-                   "clientTime":int(time.time()),
-                   "debugPhoneModel":"unknown_Android SDK built for x86_64_f62tlYkBQqOLKeWO55-vut"}
+                   "platformVersion":"29",
+                   "locale":"sv_SV",
+                   "clientTime":int(time.time()*1e3),
+                   "debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
 
     headers = {
         "accept": "application/json",
@@ -112,9 +133,12 @@ def search_by_query(query, token, user_id):
     return result
 
 def get_user_id_and_cars(token):
+    '''
+    Returns the user_id and a dict of cars the user has. The car list contains the `id` and `licenseNbr` of the cars.
+    '''
     url = "https://api.parkster.se/api/mobile/v2/people/login"
 
-    querystring = {"version":"321","platform":"android","platformVersion":"29","locale":"en_US","clientTime":int(time.time()),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
+    querystring = {"version":"321","platform":"android","platformVersion":"29","locale":"sv_SV","clientTime":int(time.time()*1e3),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
 
     headers = {
         "accept": "application/json",
@@ -131,10 +155,13 @@ def get_user_id_and_cars(token):
     return user_id, cars
 
 def search_by_location(lat,long,token,user_id):
+    '''
+    Returns a list of the parking zones near the given `lat` and `long`. Each element is a tuple with `(zone_name, zone_adress, zone_id)`
+    '''
     url = "https://api.parkster.se/api/mobile/v2/parking-zones/location-search"
 
     querystring = {"searchLat":lat,"searchLong":long,"userLat":lat,"userLong":long,"radius":"250","countryCode":"SE","userId":user_id,
-    "version":"321","platform":"android","platformVersion":"29","locale":"en_US","clientTime":int(time.time()),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
+    "version":"321","platform":"android","platformVersion":"29","locale":"sv_SV","clientTime":int(time.time()*1e3),"debugPhoneModel":"Genymobile_Google Pixel 3_cT-CAsh8Qg63-aRpE5NFDZ"}
 
     headers = {
         "accept": "application/json",
@@ -159,6 +186,9 @@ def search_by_location(lat,long,token,user_id):
     return results
 
 def buy_from_location(email, password, lat,long, license_plate=None):
+    '''
+    Buys a ticket for the location closes to the given `lat` and `long`. Picks the first car of the user if `license_plate` is not provided.
+    '''
     token = get_token(email, password)
     user_id, cars = get_user_id_and_cars(token)
     if license_plate is None:
@@ -179,6 +209,9 @@ def buy_from_location(email, password, lat,long, license_plate=None):
     buy(zone_id, feezone_id,car_id, token, user_id)
 
 def buy_from_search(email, password, query, license_plate=None):
+    '''
+    Buys a ticket for the first seach result from `query`. Picks the first car of the user if `license_plate` is not provided.
+    '''
     token = get_token(email, password)
     user_id, cars = get_user_id_and_cars(token)
     if license_plate is None:
@@ -195,6 +228,13 @@ def buy_from_search(email, password, query, license_plate=None):
     search_results = search_by_query(query,token,user_id)
     zone_name, zone_adress, zone_id = search_results[0]
     feezone_id = get_fee_id(zone_id, token)
+    print('Buying ticket at', zone_name, zone_adress)
     buy(zone_id,feezone_id, car_id, token, user_id)
 
-buy_from_search('martin@sjoborg.org', password, query='Fisksätra', license_plate=None)
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    import os
+    load_dotenv()
+    buy_from_search(os.getenv('PARKSTER_USERNAME'), os.getenv('PARKSTER_PASSWORD'), query='Hägerstensåsens BP', license_plate=None)
+    get_latest_ticket(get_token(os.getenv('PARKSTER_USERNAME'), os.getenv('PARKSTER_PASSWORD')))
