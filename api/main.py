56204001e03e5d2bef5e57709ebc1f59dc2d238e
Martin Sjöborg <martin@sjoborg.org>
2021-12-10 23:20:13 +0100

    Added input validation and return codes

--- api/main.py
+++ api/main.py
@@ -3,63 +3,61 @@ from api import flaskapp
 from api.flowbird import login_and_buy as flowbird_buy
 from api.easypark import login_and_buy as easypark_buy
 from api.parkster import buy_from_location as parkster_buy
+from marshmallow import Schema, fields, ValidationError
+
+class UserSchema(Schema):
+    username = fields.Email(required=True, error_messages={"required": {"message": "Username required in the form of an email adress.", "code": 400}})
+    password = fields.String(required=True, error_messages={"required": {"message": "Password required", "code": 400}})
+    start_time = fields.String(required=False)
+    duration = fields.String(required=False)
+    lat = fields.String(required=True, error_messages={"required": {"message": "Lat required", "code": 400}})
+    lon = fields.String(required=True, error_messages={"required": {"message": "Lon required", "code": 400}})
+
 
 @flaskapp.route("/flowbird", methods=["POST"])
 def flowbird():
     status = None
-    order_dict = {
-        "username": flask.request.form.get('username'),
-        "password": flask.request.form.get('password'),
-        "start_time": flask.request.form.get('start_time'),
-        "duration": flask.request.form.get('duration'),
-        "lat": flask.request.form.get('lat'),
-        "lon": flask.request.form.get('lon'),
-    }
+    try:
+        order_dict = UserSchema().load(flask.request.form.to_dict())
+    except ValidationError as err:
+        return err.messages, 400
     try:
         status = flowbird_buy(order_dict['username'], order_dict['password'],order_dict['lat'], order_dict['lon'],order_dict['start_time'], order_dict['duration'])
     except AssertionError as e:
-        text = 'Error: ' + str(e)
+        return 'Error: ' + str(e), 500
     if status == 'confirmed':
-        text = f'Flowbird ticket at {order_dict["lat"], order_dict["lon"]} confirmed.'
+        return f'Flowbird ticket at {order_dict["lat"], order_dict["lon"]} confirmed.', 200
     else:
-        text = f'Failed. Status is {status}'
-    return text
+        return f'Failed. Status is {status}', 500
 
 @flaskapp.route("/easypark", methods=["POST"])
 def easypark():
     status = None
-    order_dict = {
-        "username": flask.request.form.get('username'),
-        "password": flask.request.form.get('password'),
-        "duration": flask.request.form.get('duration'),
-        "lat": float(flask.request.form.get('lat')),
-        "lon": float(flask.request.form.get('lon')),
-    }
+    try:
+        order_dict = UserSchema().load(flask.request.form.to_dict())
+    except ValidationError as err:
+        return err.messages, 400
     try:
         status = easypark_buy(order_dict['username'], order_dict['password'],order_dict['lat'], order_dict['lon'])
     except AssertionError as e:
-        text = 'Error: ' + str(e)
+        return 'Error: ' + str(e), 500
     if status == 'confirmed':
-        text = f'Easypark ticket at {order_dict["lat"], order_dict["lon"]} confirmed.'
+        return f'Easypark ticket at {order_dict["lat"], order_dict["lon"]} confirmed.', 200
     else:
-        text = f'Failed. Status is {status}'
-    return text
+        return f'Failed. Status is {status}', 500
 
 @flaskapp.route("/parkster", methods=["POST"])
 def parkster():
     status = None
-    order_dict = {
-        "username": flask.request.form.get('username'),
-        "password": flask.request.form.get('password'),
-        "lat": float(flask.request.form.get('lat')),
-        "lon": float(flask.request.form.get('lon')),
-    }
+    try:
+        order_dict = UserSchema().load(flask.request.form.to_dict())
+    except ValidationError as err:
+        return err.messages, 400
     try:
         status = parkster_buy(order_dict['username'], order_dict['password'],order_dict['lat'], order_dict['lon'])
     except AssertionError as e:
-        text = 'Error: ' + str(e)
+        return 'Error: ' + str(e), 500
     if status == 'confirmed':
-        text = f'Parkster ticket at {order_dict["lat"], order_dict["lon"]} confirmed.'
+        return f'Parkster ticket at {order_dict["lat"], order_dict["lon"]} confirmed.', 200
     else:
-        text = f'Failed. Status is {status}'
-    return text
+        return f'Failed. Status is {status}', 500
