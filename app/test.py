46ff2b56cbe4ef404e503e5ebe12b1e5e3f44f96
Martin Sjöborg <martin@sjoborg.org>
2021-12-21 19:22:59 +0100

    t

--- app/test.py
+++ app/test.py
@@ -29,33 +29,16 @@ def test1():
     import tensorflow as tf
     mnist_dataset = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist_dataset.load_data()
-    # Save image parameters to the constants that we will use later for data re-shaping and for model traning.
-    (_, IMAGE_WIDTH, IMAGE_HEIGHT) = x_train.shape
-    IMAGE_CHANNELS = 1
-    x_train_with_chanels = x_train.reshape(
-    x_train.shape[0],
-    IMAGE_WIDTH,
-    IMAGE_HEIGHT,
-    IMAGE_CHANNELS
-    )
-
-    x_test_with_chanels = x_test.reshape(
-        x_test.shape[0],
-        IMAGE_WIDTH,
-        IMAGE_HEIGHT,
-        IMAGE_CHANNELS
-    )
-    x_train_normalized = x_train_with_chanels / 255
-    x_test_normalized = x_test_with_chanels / 255
-
-    addr = 'http://localhost:5000'
+    img = pickle.dumps(x_test[0])
+    #addr = 'http://localhost:5000'
+    addr = 'https://stockholm-parker.herokuapp.com'
     test_url = addr + '/photo'
 
     # prepare headers for http request
     content_type = 'image/jpeg'
     headers = {'content-type': content_type}
     response = requests.post(test_url, data=
-    pickle.dumps(x_test_normalized[0].reshape(1,28,28,1)), headers=headers)
+    img, headers=headers)
 
     print(response.text)
 test1()
